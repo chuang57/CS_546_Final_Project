a689ea5c344da89ad584a739e4fa7fed1b8a5384
jjun <jjun@stevens.edu>
2022-04-10 11:44:43 -0400

    basic form

---
+++ middlewares/auth.js
@@ -0,0 +1,9 @@
+const auth = (req, res, next) => {
+  if (!req.session.user) {
+    res.status(403).render("notLogin");
+    return;
+  }
+  next();
+};
+
+module.exports = { auth };
---
+++ routers/index.js
@@ -0,0 +1,11 @@
+
+const citysRoutes = require("./citys");
+
+const constructorMethod = (app) => {
+  app.use("/citys", citysRoutes);
+  app.use("*", (req, res) => {
+    res.status(404).json({ error: "Not found" });
+  });
+};
+
+module.exports = constructorMethod;
