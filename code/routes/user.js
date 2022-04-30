71b4e204e432d65dddce2f854bd61389889bab8e
Aditi Gupta <98054163+agupta2130@users.noreply.github.com>
2022-04-30 03:01:46 -0400

    fixed profile page and displaying all users rating  on another page.

--- code/routes/user.js
+++ code/routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createUser, checkUser } = require("../data/users");
+const { createUser, checkUser, getReviewfromId } = require("../data/users");
 const mongoCollections = require("../config/mongoCollections");
 const { ObjectId } = require("mongodb");
 const { isLogin } = require("../middleware/auth");
@@ -62,11 +62,42 @@ router.get("/signup", async (req, res) => {
 });
 
 
-router.get("/checkAllReviews", async (req, res) => {
+router.get("/profile/:email/checkAllReviews", async (req, res) => {
+  let arr = [];
   try {
-  res.render("checkAllReviews", {
-    signup: "User Signup",
+    const email = req.params.email;
+    const userCollections = await users(); 
+    const findemail2 = await userCollections.findOne({
+      email,
+    });
+
+    let genderCheck2 = ''; 
+    if(findemail2.gender === 'female')
+    {
+      genderCheck2 = 'female'
+    }
+    console.log("findemail2",findemail2.gender);
+    console.log("genderCheck2",genderCheck2);
+
+    let reviewLength2 = findemail2.reviewsWritten.length;
+  for(let i=0;i<reviewLength2;i++){
+   
+   let r2 = await getReviewfromId(findemail2.reviewsWritten[i])
+   arr.push(r2);
+   //console.log("r2...........",r2);
+  // console.log("r2.rating...........",r2);
+  
+  }
+ console.log(arr)
+   res.render("checkAllReviews", {
+    ...findemail2,
+    arr,
+    genderCheck2,
+     
   });
+
+ //console.log("findemail2",findemail2);
+  
 }catch(e)
 {
   res.send(e);
@@ -88,7 +119,10 @@ router.get("/checkAllApartments", async (req, res) => {
 });
 
 router.get("/profile/:email", async (req, res) => {
-
+  
+  let positiveRatingCount = 0;
+  let negativeRatingCount = 0;
+  let neutralRatingCount = 0;
   const email = req.params.email;
   const userCollections = await users();
 
@@ -109,6 +143,28 @@ router.get("/profile/:email", async (req, res) => {
   console.log("findemail.gender",findemail.gender); */
 
   let reviewLength = findemail.reviewsWritten.length;
+  for(let i=0;i<reviewLength;i++){
+   
+   let r = await getReviewfromId(findemail.reviewsWritten[i])
+   //console.log("r...........",r);
+   console.log("r.rating...........",r.rating);
+
+   if(r.rating === "5" || r.rating === "4"){
+    positiveRatingCount = positiveRatingCount + 1;
+   } 
+   else if(r.rating === "3"){
+    neutralRatingCount = neutralRatingCount + 1;
+   }else{
+     negativeRatingCount = negativeRatingCount + 1;
+   }
+
+  }
+
+ let postiveRatingRatio = (positiveRatingCount/reviewLength)*100;
+ let negativeRatingRatio = (negativeRatingCount/reviewLength)*100;
+ let neutralRatingRatio = (neutralRatingCount/reviewLength)*100;
+
+
   let apartmentLength = findemail.savedApartments.length;
   let genderCheck ; 
   if(findemail.gender === 'female')
@@ -124,7 +180,14 @@ router.get("/profile/:email", async (req, res) => {
     allApartmentListing,
     reviewLength,
     apartmentLength,
-    genderCheck
+    genderCheck,
+    positiveRatingCount,
+    neutralRatingCount,
+    negativeRatingCount,
+    postiveRatingRatio,
+    negativeRatingRatio,
+    neutralRatingRatio
+
   });
   return;
 });
